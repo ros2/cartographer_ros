49454e657933d52688f061d18cb11b0767870dfc
Darby Lim <thlim@robotis.com>
2019-01-06 19:57:43 +0900

    add code to transfer node_handle

--- cartographer_ros/cartographer_ros/node_main.cc
+++ cartographer_ros/cartographer_ros/node_main.cc
@@ -61,7 +61,8 @@ void Run() {
       cartographer::common::make_unique<cartographer::mapping::MapBuilder>(
           node_options.map_builder_options);
 
-  Node node(node_options, std::move(map_builder), &tf_buffer);
+  Node node(node_handle, node_options, std::move(map_builder), &tf_buffer);
+
   if (!FLAGS_load_state_filename.empty()) {
     node.LoadState(FLAGS_load_state_filename, FLAGS_load_frozen_state);
   }
