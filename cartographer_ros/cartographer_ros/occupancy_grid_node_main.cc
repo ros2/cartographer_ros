0f1b58b525f9d8e9ff019a3096e7086f10a09d57
Darby Lim <thlim@robotis.com>
2019-01-21 16:48:59 +0900

    delete publish occupancy grid

--- cartographer_ros/cartographer_ros/occupancy_grid_node_main.cc
+++ cartographer_ros/cartographer_ros/occupancy_grid_node_main.cc
@@ -63,9 +63,6 @@ class Node {
  private:
   void HandleSubmapList(const cartographer_ros_msgs::msg::SubmapList::SharedPtr msg);
   void DrawAndPublish(void);
-  // void PublishOccupancyGrid(const std::string& frame_id, const ::rclcpp::Time& time,
-  //                           const Eigen::Array2f& origin,
-  //                           cairo_surface_t* surface);
 
   ::rclcpp::Node::SharedPtr node_handle_;
   const double resolution_;
@@ -152,9 +149,6 @@ void Node::HandleSubmapList(const cartographer_ros_msgs::msg::SubmapList::Shared
         fetched_texture->pixels.intensity, fetched_texture->pixels.alpha,
         fetched_texture->width, fetched_texture->height,
         &submap_slice.cairo_data);
-        // cartographer_ros::DrawTexture(fetched_texture->pixels.intensity,
-        //             fetched_texture->pixels.alpha, fetched_texture->width,
-        //             fetched_texture->height, &submap_slice.cairo_data);
   }
 
   // Delete all submaps that didn't appear in the message.
@@ -181,52 +175,7 @@ void Node::DrawAndPublish(void)
       painted_slices, resolution_, last_frame_id_, last_timestamp_);
 
   occupancy_grid_publisher_->publish(*msg_ptr);
-  // PublishOccupancyGrid(last_frame_id_, last_timestamp_, painted_slices.origin,
-  //                      painted_slices.surface.get());
 }
-
-// void Node::PublishOccupancyGrid(const std::string& frame_id, 
-//                                 const ::rclcpp::Time& time,
-//                                 const Eigen::Array2f& origin,
-//                                 cairo_surface_t* surface) 
-// {
-//   // nav_msgs::OccupancyGrid occupancy_grid;
-//   ::nav_msgs::msg::OccupancyGrid occupancy_grid;
-//   const int width = cairo_image_surface_get_width(surface);
-//   const int height = cairo_image_surface_get_height(surface);
-//   occupancy_grid.header.stamp = time;
-//   occupancy_grid.header.frame_id = frame_id;
-//   occupancy_grid.info.map_load_time = time;
-//   occupancy_grid.info.resolution = resolution_;
-//   occupancy_grid.info.width = width;
-//   occupancy_grid.info.height = height;
-//   occupancy_grid.info.origin.position.x = -origin.x() * resolution_;
-//   occupancy_grid.info.origin.position.y = (-height + origin.y()) * resolution_;
-//   occupancy_grid.info.origin.position.z = 0.;
-//   occupancy_grid.info.origin.orientation.w = 1.;
-//   occupancy_grid.info.origin.orientation.x = 0.;
-//   occupancy_grid.info.origin.orientation.y = 0.;
-//   occupancy_grid.info.origin.orientation.z = 0.;
-
-//   const uint32_t* pixel_data =
-//       reinterpret_cast<uint32_t*>(cairo_image_surface_get_data(surface));
-//   occupancy_grid.data.reserve(width * height);
-//   for (int y = height - 1; y >= 0; --y) {
-//     for (int x = 0; x < width; ++x) {
-//       const uint32_t packed = pixel_data[y * width + x];
-//       const unsigned char color = packed >> 16;
-//       const unsigned char observed = packed >> 8;
-//       const int value =
-//           observed == 0
-//               ? -1
-//               : ::cartographer::common::RoundToInt((1. - color / 255.) * 100.);
-//       CHECK_LE(-1, value);
-//       CHECK_GE(100, value);
-//       occupancy_grid.data.push_back(value);
-//     }
-//   }
-//   occupancy_grid_publisher_->publish(occupancy_grid);
-// }
 
 }  // namespace
 }  // namespace cartographer_ros
